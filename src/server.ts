5c1bec961ca4ff42bd54ba61aff82647c1f188dc
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-06-22 11:39:54 +0600

    Now I am learnning how can handle Http request response method req method

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server: Server = createServer((req: IncomingMessage, res) => {
-  console.log(req);
-});
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === "/" && method === "GET") {
+      res.writeHead(200, { "content-type": "Application/JSON" });
+      res.end(JSON.stringify({ message: "This root route" }));
+    } else if (url?.startsWith("/products")) {
+      res.writeHead(200, { "content-type": "Application/JSON" });
+      res.end(JSON.stringify({ message: "Here is our products" }));
+    } else {
+      res.writeHead(404, { "content-type": "Application/JSON" });
+      res.end(JSON.stringify({ message: "NO page found" }));
+    }
+  },
+);
 
 server.listen(5000, () => {
   console.log("Server is running the port 50000");
